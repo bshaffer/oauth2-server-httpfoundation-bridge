bab999ae20846692e00983a9984f47590f8d1a4c
Brent Shaffer <bshafs@gmail.com>
2013-06-09 12:42:22 -0600

    Revert "upgrades for namespacing in oauth2-server-php"
    This reverts commit 5050fe499d59fbef21ab315866bed8a3a897cb91.

--- src/OAuth2/HttpFoundationBridge/Server.php
+++ src/OAuth2/HttpFoundationBridge/Server.php
@@ -3,22 +3,19 @@
 namespace OAuth2\HttpFoundationBridge;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
-use OAuth2\Server as OAuth2Server;
-use OAuth2\RequestInterface;
-use OAuth2\ResponseInterface;
 
 /**
  *
  */
- class Server extends OAuth2Server
+ class Server extends \OAuth2_Server
  {
-    public function handleTokenRequest(RequestInterface $request)
+    public function handleTokenRequest(\OAuth2_RequestInterface $request)
     {
         $response =  parent::handleTokenRequest($request);
         return $this->createResponse($response);
     }
 
-    public function handleAuthorizeRequest(RequestInterface $request, $is_authorized, $user_id = null)
+    public function handleAuthorizeRequest(\OAuth2_RequestInterface $request, $is_authorized, $user_id = null)
     {
         $response = parent::handleAuthorizeRequest($request, $is_authorized, $user_id);
         return $this->createResponse($response);
@@ -29,7 +26,7 @@ use OAuth2\ResponseInterface;
         return $this->createResponse(parent::getResponse());
     }
 
-    private function createResponse(ResponseInterface $response)
+    private function createResponse(\OAuth2_Response $response)
     {
         return new JsonResponse($response->getParameters(), $response->getStatusCode(), $response->getHttpHeaders());
     }
