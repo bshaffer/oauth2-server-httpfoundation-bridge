82aec77c5d63b968ec67d16714e4bd209f28f438
Alan Sierchio <asierchio@concoursemedia.com>
2014-07-31 15:39:03 -0400

    -bug fix for Authorization header due to apache
    See: https://github.com/bshaffer/oauth2-server-php/issues/433

--- src/OAuth2/HttpFoundationBridge/Request.php
+++ src/OAuth2/HttpFoundationBridge/Request.php
@@ -39,4 +39,41 @@ use OAuth2\RequestInterface;
     {
         return new static($request->query->all(), $request->request->all(), $request->attributes->all(), $request->cookies->all(), $request->files->all(), $request->server->all(), $request->getContent());
     }
+    
+    /**
+     * Creates a new request with values from PHP's super globals. 
+     * Overwrite to fix an apache header bug. Read more here:
+     * http://stackoverflow.com/questions/11990388/request-headers-bag-is-missing-authorization-header-in-symfony-2%E2%80%94
+     *
+     * @return Request A new request
+     *
+     * @api
+     */
+    public static function createFromGlobals()
+    {
+        $request = parent::createFromGlobals();
+        
+        //fix the bug.
+        self::fixAuthHeader($request->headers);
+        
+        return $request;
+    }
+    
+    /**
+     * PHP does not include HTTP_AUTHORIZATION in the $_SERVER array, so this header is missing.
+     * We retrieve it from apache_request_headers()
+     *
+     * @param HeaderBag $headers
+     */
+    private static function fixAuthHeader(\Symfony\Component\HttpFoundation\HeaderBag $headers)
+    {
+        if (!$headers->has('Authorization') && function_exists('apache_request_headers')) 
+        {
+            $all = apache_request_headers();
+            if (isset($all['Authorization']))
+            {
+                $headers->set('Authorization', $all['Authorization']);
+            }
+        }
+    }
  }
